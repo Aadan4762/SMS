7d691a3416a4c9000596b664daf3d182f93ed141
Aadan4762 <aadan4762@gmail.com>
2024-08-08 12:31:08 +0300

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SMS.Entities;
+
+namespace SMS.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<School> Schools { get; set; }
+}
